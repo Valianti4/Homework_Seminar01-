ccc52cd4fec823270f2844a4a95ac16ddccde06f
Klim <klim@yandex.ru>
2022-08-07 06:31:10 +0400

    Выполнил Задачу №4

--- Example04/Example04.cs
+++ Example04/Example04.cs
@@ -5,3 +5,22 @@
 // 22 3 9 -> 22
 
 
+Console.WriteLine("Введите первое целое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Первое число = ");
+Console.WriteLine(a);
+Console.Write("Второе число = ");
+Console.WriteLine(b);
+Console.Write("Третье число = ");
+Console.WriteLine(c);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("Я нашла для вас максимальное число. Им является число ");
+Console.WriteLine(max);
+Console.Write("Хорошего дня!:) ");
