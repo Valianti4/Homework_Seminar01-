bebfc0949cdc7857671c1344ae6e61650adc8044
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-08-19 05:01:05 +0400

    Внёс поправки в  задачу 29

--- Practice04/Example29/Example29.cs
+++ Practice04/Example29/Example29.cs
@@ -16,7 +16,7 @@ void ShowArray(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     { 
-        if (i < array.Length-1) Console.Write(array[i] + ",");
+        if (i < array.Length-1) Console.Write($"{array[i]}," );
         else Console.Write(array[i]);
     }
     Console.Write("]");
