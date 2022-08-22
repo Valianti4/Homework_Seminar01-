1d15ea51c1b30d0b680cbc63e1a4e6aa810a5523
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-08-22 06:17:49 +0400

    Решил задачу 34

---
+++ Practice05/Example34/Example34.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 34: Задайте массив 
+заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int even = 0;
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (i < array.Length-1) Console.Write($"{array[i]}, " );
+        else Console.Write(array[i]);
+        if (array[i] % 2 == 0) even++;                               
+    }
+    Console.Write("]");
+    Console.Write($" В массиве чётных чисел: {even}." );
+}
+
+Console.WriteLine("");
+int[] arr = CreateArray(4, 100, 999);
+PrintArray(arr);
