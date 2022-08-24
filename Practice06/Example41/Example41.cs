d0bb40331011525462a476a690ee8702813591e8
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-08-24 06:28:15 +0400

    Решил задачу 41

---
+++ Practice06/Example41/Example41.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.WriteLine("Введите несколько целых чисел, а затем нажмите клавишу Enter, чтобы узнать, какие из них больше 0.");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+int num = 0;
+
+void PrintNumbers()
+{ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) num++;
+}
+Console.WriteLine($"Количество чисел больше 0 равно {num}.");
+}
+PrintNumbers();
