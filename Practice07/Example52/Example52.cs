c0ab10d0b73f54a6fd7d6c443fbe44940949ef8a
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-08-28 19:33:53 +0400

    Решил задачу 52

---
+++ Practice07/Example52/Example52.cs
@@ -0,0 +1,60 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+
+int[,] CreateMatrix (int row, int col, int min, int max) 
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+   
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max +1);
+        }
+    }
+    return matrix;
+} 
+
+void PrintMatrix(int[,] matrix) 
+{
+    for (int i = 0; i <  matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            if (j < matrix.GetLength(1) -1) Console.Write($"{matrix[i, j], 3}, ");
+            else Console.Write($"{matrix[i, j], 3} ");
+        }
+        Console.WriteLine("]");
+    }
+        
+}
+
+void ColMeanMatrix(int[,] matrix)
+{
+    Console.Write($"Среднее арифметическое каждого столбца: ");
+    double res = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+            res = sum / matrix.GetLength(0);            
+        }        
+        Console.Write($"{Math.Round(res, 1)}. ");
+    }    
+}   
+
+int[,] array2D = CreateMatrix(5, 4, 0, 99);
+PrintMatrix(array2D);
+ColMeanMatrix(array2D);
