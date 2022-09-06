d6eab42be631983a70af56e363c91261d83faa60
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-09-07 00:16:18 +0400

    Сделал задачу 66

--- Practice09/Example66/Example66.cs
+++ Practice09/Example66/Example66.cs
@@ -5,3 +5,17 @@ M = 4; N = 8. -> 30
 */
 
 
+Console.WriteLine("Чтобы узнать сумму натуральных чисел между числами M и N, введите целое положительное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чтобы узнать сумму натуральных чисел между числами M и N, введите целое положительное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;
+    else if (N == 0) return (M * (M + 1)) / 2;
+    else if (M == N) return M;
+    else if (M < N) return N + SumNumbers(M, N - 1);
+    else return N + SumNumbers(M, N + 1);
+}
+Console.WriteLine($"Сумма натуральных чисел между {M} и {N} равна {SumNumbers(M, N)}.");
